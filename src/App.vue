076a4f0370df049e42738fc38184962be08bcefe
Lucas <lucas.81.gabriel@gmail.com>
2023-12-02 21:16:15 -0300

    🎈 perf:

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,21 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from "vue-router";
+import Menu from "./views/menu/Menu.vue";
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div class="grid grid-cols-6 h-screen">
+    <div
+      class="col-span-6 md:col-span-1 bg-stone-100 shadow-2xl border-b-2 md:border-r border-slate-300"
+    >
+      <Menu></Menu>
     </div>
-  </header>
-
-  <RouterView />
+    <div class="col-span-6 md:col-span-5 bg-zinc-100">
+      <router-view v-slot="{ Component }">
+        <transition>
+          <component :is="Component" />
+        </transition>
+      </router-view>
+    </div>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
---
+++ src/views/admin/dashboard/Dashboard.vue
@@ -0,0 +1,16 @@
+<script>
+export default{
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+<template>
+    <div class="r">
+        <div class="col-12">
+            dashboard
+        </div>
+    </div>
+</template>
