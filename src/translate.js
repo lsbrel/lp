689f8e97e3f7bb2e241e73d9e7a04e861b91402d
Lucas <123523359+lsbrel@users.noreply.github.com>
2023-12-12 11:45:20 -0300

    tradutor

--- src/translate.js
+++ src/translate.js
@@ -4,6 +4,6 @@ import pt from "./locales/pt.json";
 export default class Translate {
   static to(key) {
     const lang = "pt"; // colocal o lang no local storage
-    return eval(lang)[key];
+    return eval(lang)[key]; // EVAL precisa ser trocado é perigoso
   }
 }
