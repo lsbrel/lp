e68a78a6dcd4ebbc8e96c38c9fe4421dac8adb01
Lucas <123523359+lsbrel@users.noreply.github.com>
2023-12-12 16:36:46 -0300

    getters

--- src/stores/index.js
+++ src/stores/index.js
@@ -5,6 +5,11 @@ export const useAppStore = defineStore("app", {
   state: () => ({
     lang: "pt",
     isDarkMode: false,
+    user: {
+      id: 0,
+      type: 0,
+      name: "not-found",
+    },
   }),
   actions: {
     setLang(newLang) {
@@ -15,13 +20,13 @@ export const useAppStore = defineStore("app", {
       this.isDarkMode = !this.isDarkMode;
       LocalStorageController.storeTheme(this.isDarkMode);
     },
+    setUserData(newUserData) {
+      this.user = newUserData;
+    },
   },
   getters: {
-    getLang() {
-      return this.lang;
-    },
-    getThemeMode() {
-      return this.isDarkMode;
-    },
+    getLang: () => this.lang,
+    getThemeMode: () => this.isDarkMode,
+    getUserData: () => this.user,
   },
 });
