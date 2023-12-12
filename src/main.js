6649251b4f59d0a352411d91d5531aef315b7122
Lucas <123523359+lsbrel@users.noreply.github.com>
2023-12-12 11:37:41 -0300

    ✨ feat: tradutor caseiro

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import "./assets/main.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import Translate from "../translate";
+import Translate from "./translate";
 
 const app = createApp(App);
 
---
+++ src/translate.js
@@ -0,0 +1,9 @@
+import en from "./locales/en.json";
+import pt from "./locales/pt.json";
+
+export default class Translate {
+  static to(key) {
+    const lang = "pt"; // colocal o lang no local storage
+    return eval(lang)[key];
+  }
+}
