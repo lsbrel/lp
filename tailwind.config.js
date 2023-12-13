aeed4c63657ecc9c16cddd3d36029f878ae3338d
Lucas <123523359+lsbrel@users.noreply.github.com>
2023-12-13 11:26:37 -0300

    🌈 style: adicionando configurações de cores nas configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 // tailwind.config.js
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      primary: "blue",
+      secondary: "purple",
+      success: "green",
+      danger: "red",
+      warning: "yellow",
+      opaque: "grey"
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
