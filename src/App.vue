1dcf05f0d93055527af1df25faf554be6b8dbb43
Lucas <lucas.81.gabriel@gmail.com>
2023-12-14 00:10:32 -0300

    ✨ feat: tradutor

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import { RouterView } from "vue-router";
 import Menu from "./views/menu/Menu.vue";
+import TranslatorWidget from "./components/TranslatorWidget.vue";
 </script>
 
 <template>
   <div class="flex flex-col md:flex-row w-full md:w-scren h-screen">
     <Menu></Menu>
+    <TranslatorWidget />
     <router-view v-slot="{ Component }">
       <transition>
         <component :is="Component" />
--- src/views/admin/dashboard/Dashboard.vue
+++ src/views/admin/dashboard/Dashboard.vue
@@ -1,41 +1,13 @@
 <script>
-import { useAppStore } from "../../../stores";
-
 export default {
   data() {
-    return {
-      stores: useAppStore(),
-    };
+    return {};
   },
-  methods: {
-    changeLang(lang) {
-      this.stores.setLang(lang);
-    },
-  },
+  methods: {},
 };
 </script>
 <template>
   <div class="w-full md:w-10/12 2xl:w-11/12 h-full bg-kwhite p-5">
-    <div class="">
-      <div class="col-12">{{ $t("credentialsError") }}</div>
-      <button
-        @click="changeLang('pt')"
-        class="bg-green-600 text-white shadow-lg p-2 rounded hover:bg-green-300"
-      >
-        {{ $t("portuguese") }}
-      </button>
-      <button
-        @click="changeLang('en')"
-        class="bg-red-600 text-white shadow-lg p-2 rounded hover:bg-green-300"
-      >
-        {{ $t("english") }}
-      </button>
-      <button
-        @click="changeLang('es')"
-        class="bg-yellow-500 text-white shadow-lg p-2 rounded hover:bg-green-300"
-      >
-        {{ $t("spanish") }}
-      </button>
-    </div>
+    dashboard
   </div>
 </template>
