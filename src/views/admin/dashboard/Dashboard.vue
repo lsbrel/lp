f7f061b50964d73bacc986afeff3f243ec205efe
Lucas <123523359+lsbrel@users.noreply.github.com>
2023-12-15 09:09:04 -0300

    estrutura estetica rotas

--- src/views/admin/dashboard/Dashboard.vue
+++ src/views/admin/dashboard/Dashboard.vue
@@ -16,8 +16,8 @@ export default {
   <div class="w-full h-full bg-kwhite p-5 mt-0">
     <!-- 1 -->
     <div class="flex flex-row h-2/6 gap-2 mb-3">
-      <div class="w-1/4 bg-blue-200 rounded-md">item</div>
-      <div class="w-3/4 bg-blue-300 rounded-md">item2</div>
+      <div class="w-1/4 bg-blue-200 rounded-md">ultimo evento</div>
+      <div class="w-3/4 bg-blue-300 rounded-md">grafico de alguma coisa</div>
     </div>
     <!-- 1 -->
 
@@ -29,7 +29,9 @@ export default {
 
     <!-- 3 -->
     <div class="flex flex-row h-3/6 gap-2 mb-4">
-      <div class="w-full bg-blue-200 rounded-md">item3</div>
+      <div class="w-full bg-blue-200 rounded-md">
+        relatorio/tabela de alguma coisa
+      </div>
     </div>
     <!-- 3 -->
   </div>
--- src/views/salesman/Salesmans.vue
+++ src/views/salesman/Salesmans.vue
@@ -10,4 +10,19 @@ export default {
 </script>
 <template>
   <HeaderTitle label="salesmans" />
+  <div class="w-full h-full bg-kwhite p-5 mt-0">
+    <!-- 1 -->
+    <div class="flex flex-row h-2/6 gap-2 mb-3">
+      <div class="w-3/6 bg-blue-200 rounded-md">notificacoes</div>
+      <div class="w-2/6 bg-blue-300 rounded-md">relatorios gerais</div>
+      <div class="w-1/6 bg-blue-100 rounded-md">ultima venda</div>
+    </div>
+    <!-- 1 -->
+
+    <!-- 2 -->
+    <div class="h-4/6 mb-3">
+      <div class="h-full bg-red-300 rounded-md">Tabela/lista de vendedores</div>
+    </div>
+    <!-- 2 -->
+  </div>
 </template>
