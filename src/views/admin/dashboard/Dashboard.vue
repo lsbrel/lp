d3dbbbfb67f453ef44fa207f8539bbb83a98a484
Lucas <123523359+lsbrel@users.noreply.github.com>
2023-12-15 09:30:34 -0300

    estrutura estetica

--- src/views/admin/dashboard/Dashboard.vue
+++ src/views/admin/dashboard/Dashboard.vue
@@ -16,21 +16,28 @@ export default {
   <div class="w-full h-full bg-kwhite p-5 mt-0">
     <!-- 1 -->
     <div class="flex flex-row h-2/6 gap-2 mb-3">
-      <div class="w-1/4 bg-blue-200 rounded-md">ultimo evento</div>
-      <div class="w-3/4 bg-blue-300 rounded-md">grafico de alguma coisa</div>
+      <div class="w-2/6 bg-blue-400 rounded-md">total de vendas</div>
+      <div class="w-1/6 bg-blue-200 rounded-md">numero de eventos ativos</div>
+      <div class="w-1/6 bg-blue-300 rounded-md">numero de lojas</div>
+      <div class="w-1/6 bg-blue-50 rounded-md">numero de rotas ativas</div>
+      <div class="w-1/6 bg-blue-100 rounded-md">
+        numero de vendedores ativos
+      </div>
     </div>
     <!-- 1 -->
 
     <!-- 2 -->
     <div class="h-5/6 mb-3">
       <LeafletMap />
+      <!-- mapa com todas as cidades e todas as rotas e todas as lojas -->
     </div>
     <!-- 2 -->
 
     <!-- 3 -->
-    <div class="flex flex-row h-3/6 gap-2 mb-4">
+    <div class="flex flex-row h-5/6 gap-2 mb-4">
       <div class="w-full bg-blue-200 rounded-md">
-        relatorio/tabela de alguma coisa
+        Seletor de relatorio/lista/tabela de vendas
+        <!-- Exemplo cidades/rotas/vendedores com mais vendas -->
       </div>
     </div>
     <!-- 3 -->
