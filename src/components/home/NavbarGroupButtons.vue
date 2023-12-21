ba2377cce45a861aeb54671a4e79d0d4d713bcc8
Lucas <lucas.81.gabriel@gmail.com>
2023-12-21 18:19:00 -0300

    🦄 refactor: navbar groupd items

---
+++ src/components/home/NavbarGroupButtons.vue
@@ -0,0 +1,19 @@
+<script></script>
+<template>
+    <ul class="flex flex-col lg:flex-row list-none lg:mx-auto xl:ml-auto">
+        <li class="flex items-center">
+            <button
+                class="bg-white text-gray-800 active:bg-gray-100 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3"
+                type="button" style="transition: all 0.15s ease 0s" @click="$emit('login')">
+                <i class="fas fa-arrow-alt-circle-down"></i>{{ $t("register") }}
+            </button>
+        </li>
+        <li class="flex items-center">
+            <button
+                class="bg-white text-gray-800 active:bg-gray-100 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3"
+                type="button" style="transition: all 0.15s ease 0s" @click="$emit('login')">
+                <i class="fas fa-arrow-alt-circle-down"></i>{{ $t("enter") }}
+            </button>
+        </li>
+    </ul>
+</template>
--- src/views/landing-page/home/Navbar.vue
+++ src/views/landing-page/home/Navbar.vue
@@ -2,9 +2,13 @@
 /** LIBS */
 import Request from "@lib/Request";
 /** LIBS */
+import NavbarGroupButtons from "@components/home/NavbarGroupButtons.vue"
 
 export default {
-  data() {},
+  components: {
+    NavbarGroupButtons
+  },
+  data() { },
   methods: {
     login() {
       const data = new Request();
@@ -14,59 +18,22 @@ export default {
 };
 </script>
 <template>
-  <nav
-    class="top-0 absolute z-50 w-full flex flex-wrap items-center justify-between px-2 py-3"
-  >
-    <div
-      class="container px-4 mx-auto flex flex-wrap items-center justify-between"
-    >
-      <div
-        class="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start"
-      >
-        <a
-          class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
-          href="#market"
-          >{{ $t("market") }}</a
-        >
+  <nav class="top-0 absolute z-50 w-full flex flex-wrap items-center justify-between px-2 py-3">
+    <div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
+      <div class="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+        <a class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+          href="#market">{{ $t("market") }}</a>
       </div>
-      <div
-        class="lg:flex flex-grow items-center bg-white lg:bg-transparent lg:shadow-none hidden"
-        id="example-collapse-navbar"
-      >
+      <div class="lg:flex flex-grow items-center bg-white lg:bg-transparent lg:shadow-none hidden"
+        id="example-collapse-navbar">
         <ul class="flex flex-col lg:flex-row list-none mr-auto">
           <li class="flex items-center">
-            <a
-              class="lg:text-white lg:hover:text-gray-300 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-              href="#about"
-              ><i
-                class="lg:text-gray-300 text-gray-500 far fa-file-alt text-lg leading-lg mr-2"
-              ></i>
-              {{ $t("about-us") }}</a
-            >
+            <a class="lg:text-white lg:hover:text-gray-300 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+              href="#about"><i class="lg:text-gray-300 text-gray-500 far fa-file-alt text-lg leading-lg mr-2"></i>
+              {{ $t("about-us") }}</a>
           </li>
         </ul>
-        <ul class="flex flex-col lg:flex-row list-none lg:ml-auto">
-          <li class="flex items-center">
-            <button
-              class="bg-white text-gray-800 active:bg-gray-100 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3"
-              type="button"
-              style="transition: all 0.15s ease 0s"
-              @click="login()"
-            >
-              <i class="fas fa-arrow-alt-circle-down"></i>{{ $t("register") }}
-            </button>
-          </li>
-          <li class="flex items-center">
-            <button
-              class="bg-white text-gray-800 active:bg-gray-100 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3"
-              type="button"
-              style="transition: all 0.15s ease 0s"
-              @click="login()"
-            >
-              <i class="fas fa-arrow-alt-circle-down"></i>{{ $t("enter") }}
-            </button>
-          </li>
-        </ul>
+        <NavbarGroupButtons @login="login()" />
       </div>
     </div>
   </nav>
