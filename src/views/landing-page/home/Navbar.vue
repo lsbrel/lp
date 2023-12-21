51139c9496c2c20345e434108d1278f978c5a220
Lucas <123523359+lsbrel@users.noreply.github.com>
2023-12-21 14:38:38 -0300

    ✨ feat: navbar com informações traduzidas e corrigidas

--- src/views/landing-page/home/Navbar.vue
+++ src/views/landing-page/home/Navbar.vue
@@ -1,6 +1,5 @@
 <script>
 /** LIBS */
-import Response from "@lib/Response";
 import Request from "@lib/Request";
 /** LIBS */
 
@@ -26,15 +25,9 @@ export default {
       >
         <a
           class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
-          href="https://www.creative-tim.com/learning-lab/tailwind-starter-kit#/presentation"
-          >Tailwind Starter Kit</a
-        ><button
-          class="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
-          type="button"
-          onclick="toggleNavbar('example-collapse-navbar')"
+          href="#market"
+          >{{ $t("market") }}</a
         >
-          <i class="text-white fas fa-bars"></i>
-        </button>
       </div>
       <div
         class="lg:flex flex-grow items-center bg-white lg:bg-transparent lg:shadow-none hidden"
@@ -44,44 +37,24 @@ export default {
           <li class="flex items-center">
             <a
               class="lg:text-white lg:hover:text-gray-300 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-              href="https://www.creative-tim.com/learning-lab/tailwind-starter-kit#/landing"
+              href="#about"
               ><i
                 class="lg:text-gray-300 text-gray-500 far fa-file-alt text-lg leading-lg mr-2"
               ></i>
-              Docs</a
+              {{ $t("about-us") }}</a
             >
           </li>
         </ul>
         <ul class="flex flex-col lg:flex-row list-none lg:ml-auto">
           <li class="flex items-center">
-            <a
-              class="lg:text-white lg:hover:text-gray-300 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-              href="#pablo"
-              ><i
-                class="lg:text-gray-300 text-gray-500 fab fa-facebook text-lg leading-lg"
-              ></i
-              ><span class="lg:hidden inline-block ml-2">Share</span></a
+            <button
+              class="bg-white text-gray-800 active:bg-gray-100 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3"
+              type="button"
+              style="transition: all 0.15s ease 0s"
+              @click="login()"
             >
-          </li>
-          <li class="flex items-center">
-            <a
-              class="lg:text-white lg:hover:text-gray-300 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-              href="#pablo"
-              ><i
-                class="lg:text-gray-300 text-gray-500 fab fa-twitter text-lg leading-lg"
-              ></i
-              ><span class="lg:hidden inline-block ml-2">Tweet</span></a
-            >
-          </li>
-          <li class="flex items-center">
-            <a
-              class="lg:text-white lg:hover:text-gray-300 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-              href="#pablo"
-              ><i
-                class="lg:text-gray-300 text-gray-500 fab fa-github text-lg leading-lg"
-              ></i
-              ><span class="lg:hidden inline-block ml-2">Star</span></a
-            >
+              <i class="fas fa-arrow-alt-circle-down"></i>{{ $t("register") }}
+            </button>
           </li>
           <li class="flex items-center">
             <button
